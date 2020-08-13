bc866563f80e6b3de506549a53582bb87dc689a5
GoFly233 <gofly233@163.com>
2020-08-13 20:19:25 +0800

    refactor(PicToIdJsonBean.java): 使用 Kotlin 重写

--- app/src/main/java/com/huanchengfly/tieba/api/models/PicToIdJsonBean.kt
+++ app/src/main/java/com/huanchengfly/tieba/api/models/PicToIdJsonBean.kt
@@ -1,33 +1,16 @@
-package com.huanchengfly.tieba.api.models;
+package com.huanchengfly.tieba.api.models
 
-import com.google.gson.annotations.SerializedName;
-import com.huanchengfly.tieba.post.models.ErrorBean;
+import com.google.gson.annotations.SerializedName
+import com.huanchengfly.tieba.post.models.ErrorBean
 
-import java.util.List;
+class PicToIdJsonBean : ErrorBean() {
+    val pics: List<PicBean>? = null
 
-public class PicToIdJsonBean extends ErrorBean {
-    private List<PicBean> pics;
-
-    public List<PicBean> getPics() {
-        return pics;
-    }
-
-    public static class PicBean {
+    class PicBean {
         @SerializedName("pic_id")
-        private String picId;
-        private String width;
-        private String height;
+        val picId: String? = null
+        val width: String? = null
+        val height: String? = null
 
-        public String getPicId() {
-            return picId;
-        }
-
-        public String getWidth() {
-            return width;
-        }
-
-        public String getHeight() {
-            return height;
-        }
     }
 }
