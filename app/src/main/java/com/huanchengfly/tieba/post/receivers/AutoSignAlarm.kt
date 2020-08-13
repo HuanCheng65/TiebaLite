eb5c94dea75a8df1cb28ede08b794237f30c8c3b
GoFly233 <gofly233@163.com>
2020-08-13 17:56:47 +0800

    refactor(AutoSignAlarm.java): 使用 Kotlin 重写

--- app/src/main/java/com/huanchengfly/tieba/post/receivers/AutoSignAlarm.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/receivers/AutoSignAlarm.kt
@@ -1,16 +1,16 @@
-package com.huanchengfly.tieba.post.receivers;
+package com.huanchengfly.tieba.post.receivers
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.huanchengfly.tieba.post.utils.TiebaUtil
 
-import com.huanchengfly.tieba.post.utils.TiebaUtil;
+class AutoSignAlarm : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        TiebaUtil.startSign(context)
+    }
 
-public class AutoSignAlarm extends BroadcastReceiver {
-    public static final String TAG = AutoSignAlarm.class.getSimpleName();
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        TiebaUtil.startSign(context);
+    companion object {
+        val TAG = AutoSignAlarm::class.java.simpleName
     }
 }
