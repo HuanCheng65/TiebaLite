5b4d969f4c167deb860d2a0bd119cb14c48bf0f5
HuanCheng65 <609486518@qq.com>
2022-02-06 21:33:24 +0800

    chore: DEBUG

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -2,6 +2,8 @@
 
 echo "org.gradle.java.home=$JAVA_HOME_11_X64" >> ${APPCENTER_SOURCE_DIRECTORY}/gradle.properties
 
+cat ${APPCENTER_SOURCE_DIRECTORY}/gradle.properties
+
 openssl aes-256-cbc -d -in "${APPCENTER_SOURCE_DIRECTORY}/${releaseKeyStore}.encrypted" -k $RELEASE_ENCRYPT_SECRET_KEY -md md5 >> ${APPCENTER_SOURCE_DIRECTORY}/$releaseKeyStore
 
 printf 'releaseKeyAlias=%s\nreleaseKeyPassword=%s\nreleaseKeyStore=%s\nreleaseStorePassword=%s' $releaseKeyAlias $releaseKeyPassword $releaseKeyStore $releaseStorePassword > ${APPCENTER_SOURCE_DIRECTORY}/keystore.properties
