5e229372e9c67d15a74060c652e057a808551f40
HuanCheng65 <609486518@qq.com>
2022-02-06 21:08:33 +0800

    chore: 适配 App Center

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -d -in "${releaseKeyStore}.encrypted" -k $RELEASE_ENCRYPT_SECRET_KEY -md md5 >> $releaseKeyStore
+openssl aes-256-cbc -d -in "${APPCENTER_SOURCE_DIRECTORY}${releaseKeyStore}.encrypted" -k $RELEASE_ENCRYPT_SECRET_KEY -md md5 >> ${APPCENTER_SOURCE_DIRECTORY}$releaseKeyStore
 
-printf 'releaseKeyAlias=%s\nreleaseKeyPassword=%s\nreleaseKeyStore=%s\nreleaseStorePassword=%s' $releaseKeyAlias $releaseKeyPassword $releaseKeyStore $releaseStorePassword > keystore.properties
+printf 'releaseKeyAlias=%s\nreleaseKeyPassword=%s\nreleaseKeyStore=%s\nreleaseStorePassword=%s' $releaseKeyAlias $releaseKeyPassword $releaseKeyStore $releaseStorePassword > ${APPCENTER_SOURCE_DIRECTORY}keystore.properties
 
-chmod +x ./gradlew
+chmod +x ${APPCENTER_SOURCE_DIRECTORY}gradlew
