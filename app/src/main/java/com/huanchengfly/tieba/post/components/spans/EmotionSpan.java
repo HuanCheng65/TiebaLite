79051d8d9d7b664fcd27461b62e4339b1b4310ea
RichardLuo <22096448+RichardLuo0@users.noreply.github.com>
2022-04-20 01:15:12 +0800

    fix：修复贴吧表情图片高度与字高不一致，以及只有贴吧表情的贴子底部被截断问题

--- app/src/main/java/com/huanchengfly/tieba/post/components/spans/EmotionSpan.java
+++ app/src/main/java/com/huanchengfly/tieba/post/components/spans/EmotionSpan.java
@@ -8,10 +8,11 @@ import android.text.style.ImageSpan;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class EmotionSpan extends ImageSpan {
     public static final String TAG = EmotionSpan.class.getSimpleName();
-    private int size;
+    private final int size;
 
     public EmotionSpan(Context context, @DrawableRes int resId, int size) {
         super(context, resId, ALIGN_BASELINE);
@@ -26,12 +27,26 @@ public class EmotionSpan extends ImageSpan {
     }
 
     @Override
+    public int getSize(@NonNull Paint paint, CharSequence text, int start, int end, @Nullable Paint.FontMetricsInt fm) {
+        if (fm != null) {
+            Paint.FontMetricsInt fontFm = paint.getFontMetricsInt();
+
+            fm.ascent = fontFm.top;
+            fm.descent = fontFm.bottom;
+
+            fm.top = fm.ascent;
+            fm.bottom = fm.descent;
+        }
+
+        return size;
+    }
+
+    @Override
     public void draw(@NonNull Canvas canvas, CharSequence text, int start, int end,
                      float x, int top, int y, int bottom, @NonNull Paint paint) {
         Paint.FontMetricsInt fm = paint.getFontMetricsInt();
         Drawable drawable = getDrawable();
-        int transY = (y + fm.descent + y + fm.ascent) / 2
-                - drawable.getBounds().bottom / 2;
+        int transY = y - drawable.getBounds().bottom + fm.descent;
         canvas.save();
         canvas.translate(x, transY);
         drawable.draw(canvas);
