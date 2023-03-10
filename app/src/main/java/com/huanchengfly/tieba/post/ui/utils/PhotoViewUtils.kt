ac5998d8405a849325420d01171da44e7f9c88b9
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-03-11 00:12:03 +0800

    feat: 新版贴子 - 看图

--- app/src/main/java/com/huanchengfly/tieba/post/ui/utils/PhotoViewUtils.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/utils/PhotoViewUtils.kt
@@ -1,10 +1,43 @@
 package com.huanchengfly.tieba.post.ui.utils
 
+import com.huanchengfly.tieba.post.api.models.protos.Post
 import com.huanchengfly.tieba.post.api.models.protos.ThreadInfo
 import com.huanchengfly.tieba.post.models.protos.LoadPicPageData
 import com.huanchengfly.tieba.post.models.protos.PhotoViewData
 import com.huanchengfly.tieba.post.models.protos.PicItem
 import com.huanchengfly.tieba.post.utils.ImageUtil
+
+fun getPhotoViewData(
+    post: Post,
+    picId: String,
+    picUrl: String,
+    originUrl: String,
+    showOriginBtn: Boolean,
+    originSize: Int
+): PhotoViewData? {
+    if (post.from_forum == null) return null
+    return PhotoViewData(
+        data_ = LoadPicPageData(
+            forumId = post.from_forum.id,
+            forumName = post.from_forum.name,
+            threadId = post.tid,
+            postId = post.id,
+            objType = "pb",
+            picId = picId,
+            picIndex = 1
+        ),
+        picItems = listOf(
+            PicItem(
+                picId = picId,
+                picIndex = 1,
+                url = picUrl,
+                originUrl = originUrl,
+                showOriginBtn = showOriginBtn,
+                originSize = originSize
+            )
+        )
+    )
+}
 
 fun getPhotoViewData(
     threadInfo: ThreadInfo,
