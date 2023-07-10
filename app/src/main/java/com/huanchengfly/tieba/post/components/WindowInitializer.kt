3f5f861722af239ad72a9d0821edd1fad9f280ff
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-07-10 17:59:47 +0800

    chore: 更新依赖

--- app/src/main/java/com/huanchengfly/tieba/post/components/WindowInitializer.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/components/WindowInitializer.kt
@@ -3,14 +3,15 @@ package com.huanchengfly.tieba.post.components
 import android.content.Context
 import androidx.startup.Initializer
 import androidx.window.core.ExperimentalWindowApi
-import androidx.window.embedding.SplitController
+import androidx.window.embedding.RuleController
 import com.huanchengfly.tieba.post.R
 
 @ExperimentalWindowApi
-class WindowInitializer : Initializer<SplitController> {
-    override fun create(context: Context): SplitController {
-        SplitController.initialize(context, R.xml.main_split_config)
-        return SplitController.getInstance()
+class WindowInitializer : Initializer<RuleController> {
+    override fun create(context: Context): RuleController {
+        return RuleController.getInstance(context).apply {
+            setRules(RuleController.parseRules(context, R.xml.main_split_config))
+        }
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
