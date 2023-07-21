690177a078798554d4fd3853db0f90c7818c5147
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-07-21 22:21:18 +0800

    fix: 看图页面导航栏边距

--- app/src/main/java/com/huanchengfly/tieba/post/ui/page/photoview/PhotoViewActivity.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/page/photoview/PhotoViewActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
@@ -249,7 +250,8 @@ class PhotoViewActivity : BaseComposeActivityWithParcelable<PhotoViewData>() {
                                             )
                                         )
                                     )
-                                    .padding(horizontal = 16.dp),
+                                    .padding(horizontal = 16.dp)
+                                    .navigationBarsPadding(),
                                 verticalAlignment = Alignment.CenterVertically
                             ) {
                                 val index = pagerState.currentPage
