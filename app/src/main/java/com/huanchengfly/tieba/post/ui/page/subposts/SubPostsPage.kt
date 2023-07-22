063806e6a6bd77523f6b83dd6d3f232caf7bba89
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-07-22 08:06:55 +0800

    fix: 楼中楼页面底栏被导航栏覆盖

--- app/src/main/java/com/huanchengfly/tieba/post/ui/page/subposts/SubPostsPage.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/page/subposts/SubPostsPage.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -214,7 +215,9 @@ internal fun SubPostsContent(
                         elevation = 16.dp,
                         color = ExtendedTheme.colors.bottomBar,
                         contentColor = ExtendedTheme.colors.text,
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .navigationBarsPadding()
                     ) {
                         Row(
                             modifier = Modifier
