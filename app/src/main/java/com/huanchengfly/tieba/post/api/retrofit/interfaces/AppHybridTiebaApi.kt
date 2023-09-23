fad09a4e87791967af8b4588226e464f010f88f4
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2023-09-23 14:05:32 +0800

    fix: 搜索词带中文时无法搜索

--- app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/interfaces/AppHybridTiebaApi.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/interfaces/AppHybridTiebaApi.kt
@@ -3,6 +3,7 @@ package com.huanchengfly.tieba.post.api.retrofit.interfaces
 import com.huanchengfly.tieba.post.api.models.SearchForumBean
 import com.huanchengfly.tieba.post.api.models.SearchThreadBean
 import com.huanchengfly.tieba.post.api.models.SearchUserBean
+import com.huanchengfly.tieba.post.api.urlEncode
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -12,7 +13,7 @@ interface AppHybridTiebaApi {
     @GET("/mo/q/search/forum")
     fun searchForumFlow(
         @Query("word") keyword: String,
-        @Header("Referer") referer: String = "https://tieba.baidu.com/mo/q/hybrid/search?keyword=$keyword&_webview_time=${System.currentTimeMillis()}",
+        @Header("Referer") referer: String = "https://tieba.baidu.com/mo/q/hybrid/search?keyword=$keyword&_webview_time=${System.currentTimeMillis()}".urlEncode(),
     ): Flow<SearchForumBean>
 
     @GET("/mo/q/search/thread")
@@ -23,12 +24,12 @@ interface AppHybridTiebaApi {
         @Query("tt") filter: Int = 1,
         @Query("ct") ct: Int = 1,
         @Query("cv") cv: String = "99.9.101",
-        @Header("Referer") referer: String = "https://tieba.baidu.com/mo/q/hybrid/search?keyword=$keyword&_webview_time=${System.currentTimeMillis()}",
+        @Header("Referer") referer: String = "https://tieba.baidu.com/mo/q/hybrid/search?keyword=$keyword&_webview_time=${System.currentTimeMillis()}".urlEncode(),
     ): Flow<SearchThreadBean>
 
     @GET("/mo/q/search/user")
     fun searchUserFlow(
         @Query("word") keyword: String,
-        @Header("Referer") referer: String = "https://tieba.baidu.com/mo/q/hybrid/search?keyword=$keyword&_webview_time=${System.currentTimeMillis()}",
+        @Header("Referer") referer: String = "https://tieba.baidu.com/mo/q/hybrid/search?keyword=$keyword&_webview_time=${System.currentTimeMillis()}".urlEncode(),
     ): Flow<SearchUserBean>
 }
